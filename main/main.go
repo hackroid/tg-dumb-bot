8cc6b9ed66cff2a236e220e9e1a7a168848377b5
hackroid <ooozbh@gmail.com>
2023-03-10 22:22:11 +0900

    not ignore go.sum

--- main/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
-	bs := new(BotServer)
-	bs.initBot()
-	bs.addMessageHandler(MSG_TYPE_TEXT, normalTextMessage)
-	bs.addMessageHandler(MSG_TYPE_CMD, normalCommandMessage)
-	bs.pollingChannelUpdates()
-}
