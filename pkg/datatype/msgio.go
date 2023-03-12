94efa2482e5955f2e62a45ff35ac94664f68442c
hackroid <ooozbh@gmail.com>
2023-03-12 20:48:06 +0900

    design a handler factory, extract the command pattern, and static method for easier testing

---
+++ pkg/datatype/msgio.go
@@ -0,0 +1,18 @@
+package datatype
+
+type TextContentRecv struct {
+	Text string
+}
+
+type TextContentResp struct {
+	Text string
+}
+
+type CommandContentRecv struct {
+	Cmd  string
+	Text string
+}
+
+type CommandContentResp struct {
+	Text string
+}
--- pkg/handler/msghandler.go
+++ pkg/handler/msghandler.go
@@ -2,42 +2,84 @@ package handler
 
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/hackroid/tg-dumb-bot/pkg/datatype"
+	"github.com/hackroid/tg-dumb-bot/pkg/static"
 	"log"
-	"math/rand"
-	"strings"
-	"unicode"
 )
 
-func NormalTextMessage(recvMsg *tgbotapi.Message) (string, bool, error) {
-	log.Printf("[TEXT] %s", recvMsg.Text)
-	p := rand.Intn(100)
-	if p < 15 {
-		return recvMsg.Text + "个几把", true, nil
-	}
-	return "", false, nil
+type TextMessageHandler struct {
+	recvMsg     *tgbotapi.Message
+	recvContent datatype.TextContentRecv
+	reply       bool
+	ok          bool
+	respContent datatype.TextContentResp
+	respMsg     tgbotapi.MessageConfig
 }
 
-func NormalCommandMessage(recvMsg *tgbotapi.Message) (string, bool, error) {
-	splitter := func(r rune) bool { return unicode.IsSpace(r) }
-	var msg string
-	switch recvMsg.Command() {
-	case "help":
-		msg = "按 \"/\" 自己看"
-	case "choice":
-		log.Printf("[CMD] %s", recvMsg.Text)
-		dices := strings.FieldsFunc(recvMsg.Text, splitter)
-		if len(dices) == 1 {
-			msg = "你选寄吧呢"
-		} else if len(dices) == 2 {
-			msg = "就一个你选寄吧呢"
-		} else {
-			dices = dices[1:]
-			msg = dices[rand.Intn(len(dices))]
-		}
-	case "status":
-		msg = "I'm 凹K."
-	default:
-		msg = "你说寄吧呢"
+func (h *TextMessageHandler) Extract() {
+	h.recvContent.Text = h.recvMsg.Text
+	h.reply = true
+}
+
+func (h *TextMessageHandler) Generate() {
+	h.respContent.Text, h.ok, _ = static.AddGouBa(h.recvContent)
+}
+
+func (h *TextMessageHandler) Pack() {
+	if !h.ok {
+		return
 	}
-	return msg, true, nil
+	// Start packing you msg
+	h.respMsg = tgbotapi.NewMessage(h.recvMsg.Chat.ID, h.respContent.Text)
+
+	// End packing
+	if h.reply {
+		h.respMsg.ReplyToMessageID = h.recvMsg.MessageID
+	}
+}
+
+func (h *TextMessageHandler) Send(ch chan tgbotapi.MessageConfig) {
+	if h.ok {
+		ch <- h.respMsg
+	}
+}
+
+type CommandMessageHandler struct {
+	recvMsg     *tgbotapi.Message
+	recvContent datatype.CommandContentRecv
+	reply       bool
+	ok          bool
+	respContent datatype.CommandContentResp
+	respMsg     tgbotapi.MessageConfig
+}
+
+func (h *CommandMessageHandler) Extract() {
+	h.recvContent.Text = h.recvMsg.Text
+	h.recvContent.Cmd = h.recvMsg.Command()
+	h.reply = true
+	h.ok = true
+}
+
+func (h *CommandMessageHandler) Generate() {
+	h.respContent.Text, h.ok, _ = static.NormalCommandMessage(h.recvContent)
+}
+
+func (h *CommandMessageHandler) Pack() {
+	if !h.ok {
+		return
+	}
+	// Start packing you msg
+	h.respMsg = tgbotapi.NewMessage(h.recvMsg.Chat.ID, h.respContent.Text)
+
+	// End packing
+	if h.reply {
+		h.respMsg.ReplyToMessageID = h.recvMsg.MessageID
+	}
+}
+
+func (h *CommandMessageHandler) Send(ch chan tgbotapi.MessageConfig) {
+	if h.ok {
+		log.Printf("[Replyed]\n")
+		ch <- h.respMsg
+	}
 }
